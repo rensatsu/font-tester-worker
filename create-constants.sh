a4ce5ee2c71fbba97900d28c1fefbb111ae7f311
Pavel Tvaladze <reddragonsportal@gmail.com>
2020-01-27 20:55:38 +0300

    fix build

--- create-constants.sh
+++ create-constants.sh
@@ -7,16 +7,21 @@ echo "Printing env:"
 printenv;
 
 echo "Writing constants.js"
-cat <<<EOT
-const GFONTS_API_KEY = '${GFONTS_API_KEY}';
-const GFONTS_REFERRER = '${GFONTS_REFERRER}';
-const GIST_ID = '${GIST_ID}';
+if [ -f "constants.js" ]; then
+    echo "Constats file already exists"
+    exit 1;
+fi;
+
+cat <<EOT  > constants.js
+const GFONTS_API_KEY = '${GFONTS_API_KEY:-}';
+const GFONTS_REFERRER = '${GFONTS_REFERRER:-}';
+const GIST_ID = '${GIST_ID:-}';
 
 module.exports = {
   GFONTS_API_KEY,
   GFONTS_REFERRER,
   GIST_ID,
 };
-EOT >> constants.js
+EOT
 
 echo "Completed"
