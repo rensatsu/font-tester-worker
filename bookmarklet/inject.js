3f8110ffb7d376dc31dd6b4b39c69d03e1d83626
Pavel Tvaladze <reddragonsportal@gmail.com>
2020-03-07 17:05:07 +0300

    fix formatting

--- bookmarklet/inject.js
+++ bookmarklet/inject.js
@@ -1,14 +1,10 @@
-(function () {
-    const LinkGFT = {
-        fontList: [],
-        init: function () {
-            const request = {
-                method: 'get'
-            };
-
-            const gftStyle = document.createElement('style');
-            gftStyle.id = 'link-gft-css0';
-            gftStyle.innerHTML = `
+(function() {
+  const LinkGFT = {
+    fontList: [],
+    init: function() {
+      const gftStyle = document.createElement('style');
+      gftStyle.id = 'link-gft-css0';
+      gftStyle.innerHTML = `
                 .link-gft-container {
                     position: fixed;
                     bottom: 0;
@@ -48,94 +44,172 @@
                 }
             `;
 
-            document.head.appendChild(gftStyle);
+      document.head.appendChild(gftStyle);
 
-            fetch("https://fonts-api.rencloud.workers.dev/fonts", request)
-                .then((response) => {
-                    return response.json();
-                }, (e) => {
-                    console.log("[GFT]", "Error loading Google Font Tester, fallback to predefined list", e);
-                })
-                .then((json) => {
-                    if (json && json.fonts) {
-                        this.fontList = json && json.fonts ? json.fonts : [];
-                    } else {
-                        this.fontList = ["Andika", "Anonymous Pro", "Arimo", "Bad Script", "Comfortaa", "Cormorant", "Cormorant Garamond", "Cormorant Infant", "Cormorant SC", "Cormorant Unicase", "Cousine", "Cuprum", "Didact Gothic", "EB Garamond", "El Messiri", "Exo 2", "Fira Mono", "Fira Sans", "Fira Sans Condensed", "Fira Sans Extra Condensed", "Forum", "Istok Web", "Jura", "Kelly Slab", "Kurale", "Ledger", "Lobster", "Lora", "Marck Script", "Marmelad", "Merriweather", "Neucha", "Noto Sans", "Noto Serif", "Open Sans", "Open Sans Condensed", "Oranienbaum", "PT Mono", "PT Sans", "PT Sans Caption", "PT Sans Narrow", "PT Serif", "PT Serif Caption", "Pattaya", "Philosopher", "Play", "Playfair Display", "Playfair Display SC", "Poiret One", "Press Start 2P", "Prosto One", "Roboto", "Roboto Condensed", "Roboto Mono", "Roboto Slab", "Rubik", "Rubik Mono One", "Rubik One", "Ruslan Display", "Russo One", "Scada", "Seymour One", "Stalinist One", "Tenor Sans", "Tinos", "Ubuntu", "Ubuntu Condensed", "Ubuntu Mono", "Underdog", "Yeseva One"];
-                    }
+      fetch('https://fonts-api.rencloud.workers.dev/fonts', { method: 'get' })
+        .then(
+          response => {
+            return response.json();
+          },
+          e => {
+            console.log(
+              '[GFT]',
+              'Error loading Google Font Tester, fallback to predefined list',
+              e,
+            );
+          },
+        )
+        .then(json => {
+          if (json && json.fonts) {
+            this.fontList = json && json.fonts ? json.fonts : [];
+          } else {
+            this.fontList = [
+              'Andika',
+              'Anonymous Pro',
+              'Arimo',
+              'Bad Script',
+              'Comfortaa',
+              'Cormorant',
+              'Cormorant Garamond',
+              'Cormorant Infant',
+              'Cormorant SC',
+              'Cormorant Unicase',
+              'Cousine',
+              'Cuprum',
+              'Didact Gothic',
+              'EB Garamond',
+              'El Messiri',
+              'Exo 2',
+              'Fira Mono',
+              'Fira Sans',
+              'Fira Sans Condensed',
+              'Fira Sans Extra Condensed',
+              'Forum',
+              'Istok Web',
+              'Jura',
+              'Kelly Slab',
+              'Kurale',
+              'Ledger',
+              'Lobster',
+              'Lora',
+              'Marck Script',
+              'Marmelad',
+              'Merriweather',
+              'Neucha',
+              'Noto Sans',
+              'Noto Serif',
+              'Open Sans',
+              'Open Sans Condensed',
+              'Oranienbaum',
+              'PT Mono',
+              'PT Sans',
+              'PT Sans Caption',
+              'PT Sans Narrow',
+              'PT Serif',
+              'PT Serif Caption',
+              'Pattaya',
+              'Philosopher',
+              'Play',
+              'Playfair Display',
+              'Playfair Display SC',
+              'Poiret One',
+              'Press Start 2P',
+              'Prosto One',
+              'Roboto',
+              'Roboto Condensed',
+              'Roboto Mono',
+              'Roboto Slab',
+              'Rubik',
+              'Rubik Mono One',
+              'Rubik One',
+              'Ruslan Display',
+              'Russo One',
+              'Scada',
+              'Seymour One',
+              'Stalinist One',
+              'Tenor Sans',
+              'Tinos',
+              'Ubuntu',
+              'Ubuntu Condensed',
+              'Ubuntu Mono',
+              'Underdog',
+              'Yeseva One',
+            ];
+          }
 
-                    this.runApp();
-                });
-        },
+          this.runApp();
+        });
+    },
 
-        runApp: function () {
-            if (document.querySelector('#link-gft-select') === null) {
-                gFonts = this.fontList.sort();
+    runApp: function() {
+      if (document.querySelector('#link-gft-select') === null) {
+        gFonts = this.fontList.sort();
 
-                const gftContainer = document.createElement('div');
-                gftContainer.classList.add('link-gft-container');
-                gftContainer.textContent = 'Google Fonts Tester:';
+        const gftContainer = document.createElement('div');
+        gftContainer.classList.add('link-gft-container');
+        gftContainer.textContent = 'Google Fonts Tester:';
 
-                const gftFontSelect = document.createElement('select');
-                gftFontSelect.classList.add('link-gft-select');
+        const gftFontSelect = document.createElement('select');
+        gftFontSelect.classList.add('link-gft-select');
 
-                gFonts.forEach((font) => {
-                    const option = document.createElement('option');
-                    option.value = font;
-                    option.textContent = font;
-                    gftFontSelect.append(option);
-                });
+        gFonts.forEach(font => {
+          const option = document.createElement('option');
+          option.value = font;
+          option.textContent = font;
+          gftFontSelect.append(option);
+        });
 
-                const gftNotification = document.createElement('div');
-                gftNotification.classList.add('link-gft-notification');
+        const gftNotification = document.createElement('div');
+        gftNotification.classList.add('link-gft-notification');
 
-                const gftNotificationText = document.createElement('p');
-                gftNotificationText.textContent = 'GFT is no longer supported.';
+        const gftNotificationText = document.createElement('p');
+        gftNotificationText.textContent = 'GFT is no longer supported.';
 
-                const gftNotificationLink = document.createElement('a');
+        const gftNotificationLink = document.createElement('a');
 
-                gftNotificationLink.textContent = 'Snapfont is a good alternative.';
-                gftNotificationLink.href = 'https://getsnapfont.com/';
-                gftNotificationLink.target = '_blank';
-                gftNotificationLink.rel = 'nofollow';
+        gftNotificationLink.textContent = 'Snapfont is a good alternative.';
+        gftNotificationLink.href = 'https://getsnapfont.com/';
+        gftNotificationLink.target = '_blank';
+        gftNotificationLink.rel = 'nofollow';
 
-                gftNotification.append(gftNotificationText, gftNotificationLink);
-                gftContainer.append(gftFontSelect, gftNotification);
+        gftNotification.append(gftNotificationText, gftNotificationLink);
+        gftContainer.append(gftFontSelect, gftNotification);
 
-                document.body.appendChild(gftContainer);
+        document.body.appendChild(gftContainer);
 
-                gftFontSelect.addEventListener('change', function () {
-                    const font = this.value;
+        gftFontSelect.addEventListener('change', function() {
+          const font = this.value;
 
-                    if (font.length > 0) {
-                        const fontesc = encodeURIComponent(font);
+          if (font.length > 0) {
+            const fontesc = encodeURIComponent(font);
 
-                        if (document.querySelector('#link-gft-css1') !== null) {
-                            document.querySelector('#link-gft-css1').remove();
-                        }
-
-                        if (document.querySelector('#link-gft-css2') !== null) {
-                            document.querySelector('#link-gft-css2').remove();
-                        }
-
-                        const cssLink = document.createElement('link');
-                        cssLink.href = `https://fonts.googleapis.com/css?family=${fontesc}:400,800&subset=latin,cyrillic`;
-                        cssLink.rel = 'stylesheet';
-                        cssLink.id = 'link-gft-css1';
-
-                        document.head.appendChild(cssLink);
-
-                        const cssStyle = document.createElement('style');
-                        cssStyle.innerHTML = `* {font-family: '${font}' !important;}`;
-                        cssStyle.id = 'link-gft-css2';
-
-                        document.head.appendChild(cssStyle);
-                    }
-                });
-            } else {
-                alert('Google Fonts Tester [GFT] is already loaded!');
+            if (document.querySelector('#link-gft-css1') !== null) {
+              document.querySelector('#link-gft-css1').remove();
             }
-        }
-    };
 
-    LinkGFT.init();
+            if (document.querySelector('#link-gft-css2') !== null) {
+              document.querySelector('#link-gft-css2').remove();
+            }
+
+            const cssLink = document.createElement('link');
+            cssLink.href = `https://fonts.googleapis.com/css?family=${fontesc}:400,800&subset=latin,cyrillic`;
+            cssLink.rel = 'stylesheet';
+            cssLink.id = 'link-gft-css1';
+
+            document.head.appendChild(cssLink);
+
+            const cssStyle = document.createElement('style');
+            cssStyle.innerHTML = `* {font-family: '${font}' !important;}`;
+            cssStyle.id = 'link-gft-css2';
+
+            document.head.appendChild(cssStyle);
+          }
+        });
+      } else {
+        alert('Google Fonts Tester [GFT] is already loaded!');
+      }
+    },
+  };
+
+  LinkGFT.init();
 })();
