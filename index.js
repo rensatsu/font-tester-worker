400a3a2820251122b1143426ae0d1c1ecdf96443
Pavel Tvaladze <reddragonsportal@gmail.com>
2020-03-07 17:02:51 +0300

    add comments

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const Constants = require('./constants');
 const injectScript = require('raw-loader!./bookmarklet/inject.js');
 
+/** @constant {string} CACHE_TTL Cache Time-To-Live */
 const CACHE_TTL = 1 * 24 * 3600;
 
+/** Register worker handler */
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request));
 });
 
+/** @constant {string} GFONTS_API_KEY */
 const GFONTS_API_KEY = Constants.GFONTS_API_KEY;
+
+/** @constant {string} GFONTS_REFERRER */
 const GFONTS_REFERRER = Constants.GFONTS_REFERRER;
 
+/** @constant {string} GITHUB_URL */
 const GITHUB_URL = `https://github.com/rensatsu/font-tester-worker`;
 
+/**
+ * Fetch fonts list from Google Fonts.
+ *
+ * @returns {(object|null)} JSON list of fonts or null.
+ */
 async function getFontsList() {
   try {
     const apiUrl = new URL('https://www.googleapis.com/webfonts/v1/webfonts');
@@ -37,6 +48,13 @@ async function getFontsList() {
   }
 }
 
+/**
+ * Generate an error response.
+ *
+ * @param {number} [code=404] HTTP Status Code
+ * @param {string} [text='Not found'] Text of the response
+ * @returns
+ */
 function errorResponse(code = 404, text = 'Not found') {
   const response = new Response(text, {
     status: code >= 200 && code <= 599 ? code : 500,
@@ -49,10 +67,20 @@ function errorResponse(code = 404, text = 'Not found') {
   return response;
 }
 
+/**
+ * Add CORS header to the response.
+ *
+ * @param {Response} response Response object.
+ */
 function addCors(response) {
   response.headers.set('Access-Control-Allow-Origin', '*');
 }
 
+/**
+ * Handle request to get fonts list.
+ *
+ * @returns {Response} Response object.
+ */
 async function handleGetFonts() {
   const gFontsList = await getFontsList();
 
@@ -82,6 +110,11 @@ async function handleGetFonts() {
   return response;
 }
 
+/**
+ * Handle request to get debug information.
+ *
+ * @returns {Response} Response object.
+ */
 async function handleGetDebug() {
   const jsonResp = {
     build: 'GIT_COMMIT' in Constants ? Constants.GIT_COMMIT : null,
@@ -93,6 +126,11 @@ async function handleGetDebug() {
   return response;
 }
 
+/**
+ * Handle request to get information about this app.
+ *
+ * @returns {Response} Response object.
+ */
 function handleGetInfo() {
   const response = new Response('Redirecting', { status: 302 });
   response.headers.set('Location', GITHUB_URL);
@@ -100,6 +138,11 @@ function handleGetInfo() {
   return response;
 }
 
+/**
+ * Handle request to get injector script.
+ *
+ * @returns {Response} Response object.
+ */
 async function handleGetInjectJs() {
   const gInjScript = injectScript.default;
 
